16e25ea247505999528d23d90c4b976489ab08a0
Sitanshu Mustafi <sitanshumustafi95035@gmail.com>
2025-01-07 19:54:06 +0530

    shadcn ui component setup

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <div>
       <h2>Chillar Charcha</h2>
+      <Button variant='outline'>Hello World!</Button>
     </div>
   );
 }
